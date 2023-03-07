c7d349109fd4cf4cb11102c3a3f414ecfe9906d8
yangxun3 <yangxun3@Lenovo.com>
2023-03-07 17:42:28 +0800

    ci(hooks): 测试commit hook

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+export default {
+  onLaunch: function () {
+    console.log("App Launch");
+  },
+  onShow: function () {
+    console.log("App Show");
+  },
+  onHide: function () {
+    console.log("App Hide");
+
+    console.log("app             333333333");
+  },
+};
 </script>
 
 <style>
-	/*每个页面公共css */
+/*每个页面公共css */
 </style>
